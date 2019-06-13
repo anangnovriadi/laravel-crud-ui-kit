b913afa40410403279d9e8e6629449c273cdbc68
anangnov99@gmail.com <anangnov99@gmail.com>
2019-06-13 11:14:51 +0700

    layout home

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel - UI Kit</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/css/uikit.min.css" />
+</head>
+<body>
+    @include('layout.header')
+
+    @yield('content')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/js/uikit-icons.min.js"></script>
+</body>
+</html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,7 @@
+<nav class="uk-navbar-container uk-flex-center" uk-navbar>
+    <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="" class="uk-text-bold">Laravel CRUD with UI Kit</a></li>
+        </ul>
+    </div>
+</nav>
